58a3ad9ea88812939a5c2ce291814c1ef761daac
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-17 05:59:45 +0100

    Create ChatbotController.php

---
+++ src/controller/ChatbotController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChatbotController extends AbstractController
+{
+    #[Route('/chatbot', name: 'chatbot')]
+    public function index()
+    {
+        return $this->render('mathematicians/chatbot.html.twig');
+    }
+}
