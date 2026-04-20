c8e92fa1cfeb85c82dd774c90989618bfe7c89b1
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-20 17:14:09 +0100

    controller of the second page

--- src/controller/ChatbotController.php
+++ src/controller/ChatbotController.php
@@ -5,8 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChatbotController extends AbstractController
-{
-    #[Route('/chatbot', name: 'chatbot')]
+{   #[Route('/chatbot', name: 'chatbot')]
     public function index()
     {
         return $this->render('mathematicians/chatbot.html.twig');
